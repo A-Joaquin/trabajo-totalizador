6d5a4798a69049556e75abc954548d17f028b7cd
Arturo <arturo.veliz@ucb.edu.bo>
2024-03-03 09:58:57 -0400

    IU añadida para calcular precio

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,15 @@
 import sumar from "./sumador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const first = document.querySelector("#cantidad-items");
+const second = document.querySelector("#precio-items");
+const form = document.querySelector("#totalizador-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const secondNumber = Number.parseFloat(second.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + calcularPrecio(firstNumber, secondNumber) + "</p>";
 });
