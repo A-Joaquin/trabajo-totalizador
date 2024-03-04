d34283d9c8765ea64617a4879e474e197a3c7a34
Salvador_Patty_Camacho <salvador.patty@ucb.edu.bo>
2024-03-04 00:38:09 -0400

    Añadido el calculo del precio total con el impuesto de NV

--- src/calcularPrecio.js
+++ src/calcularPrecio.js
@@ -1,10 +1,11 @@
 function calcularPrecio(cantidad,precio,estado) {
   let impuesto = 0;
-  if (estado === "CA") {
-    // Impuesto del 8.25% para California
+  if (estado === "CA") { // Impuesto del 8.25% para California
     impuesto = 0.0825;
-  } else if(estado === "UT") {
+  } else if(estado === "UT") { // Impuesto del 6.65% para Utah
     impuesto = 0.0665
+  } else if(estado === "NV") { // Impuesto del 8.00% para Nevada
+    impuesto = 0.08
   }
   const subtotal = cantidad * precio;
   const total = subtotal + subtotal * impuesto;
--- src/calcularPrecio.test.js
+++ src/calcularPrecio.test.js
@@ -6,14 +6,14 @@ describe("Calcular precio neto", () => {
   });
 
   it("Calcular precio neto con impuesto en California (CA)", () => {
-    // Supongamos que el impuesto para California es del 8.25%
     expect(calcularPrecio(5, 10, "CA")).toEqual(54.125);
   });
 
   it("Calcular precio neto con impuesto en Utah (UT)", () => {
-    // Supongamos que el impuesto para Utah es del 6.65%
     expect(calcularPrecio(20, 8, "UT")).toEqual(170.64);
   });
 
-  // Puedes agregar más pruebas para otros estados o escenarios
+  it("Calcular precio neto con impuesto en Nevada (NV)", () => {
+    expect(calcularPrecio(15, 21, "NV")).toEqual(340.2);
+  });
 });
