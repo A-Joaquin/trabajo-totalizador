ab866df0e736392f023a4e75415ab0eb45f6cdba
Salvador_Patty_Camacho <salvador.patty@ucb.edu.bo>
2024-03-04 00:08:54 -0400

    Añadida la interfaz de estados con el impuesto

--- src/calcularPrecio.test.js
+++ src/calcularPrecio.test.js
@@ -1,7 +1,10 @@
 import calcularPrecio from "./calcularPrecio";
 
-describe("Sumar", () => {
-  it("deberia de calcular precio neto cantidad*precio", () => {
+describe("Calcular precio neto", () => {
+  it("Calcular precio neto 1", () => {
     expect(calcularPrecio(3, 2)).toEqual(6);
+  });
+  it("Calcular precio neto 2", () => {
+    expect(calcularPrecio(3, 5)).toEqual(15);
   });
 });
